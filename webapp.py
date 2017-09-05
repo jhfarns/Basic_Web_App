eb6e98ee95818b77c11cbceb2ba749f986e9cb14
James Farnsworth <jfarnsworth@securitymetrics.com>
2017-09-05 14:19:23 -0600

    Added Cookie to the webapp

--- webapp.py
+++ webapp.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
-from bottle import route, run
+from bottle import route, run, request, response
+import randGen
+
+value = randGen.randNum()
 
 @route('/hello')
+@route('/')
 def hello():
-    return "Hello World!"
+    if request.get_cookie('squidge'):
+        return 'Squidge welcomes you back to Squindistries!'
+    else:
+        response.set_cookie('squidge', value)
+    return "Squidge welcomes you to Squindistries!"
+
 
 run(host='localhost', port=8080, debug=True)
