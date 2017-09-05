2842a8eb6dc89248371f448e5fe31631d1b8b102
James Farnsworth <jfarnsworth@securitymetrics.com>
2017-09-05 14:19:59 -0600

    Finished RandGenerator

--- randGen.py
+++ randGen.py
@@ -1,4 +1,5 @@
-import string,random
+import string
+import random
 
-def random (length = 8, string =  string.ascii_uppercase + string.digits):
+def randNum (length = 8, string =  string.ascii_uppercase + string.digits):
     return ''.join(random.choice(string) for a in range(length))
