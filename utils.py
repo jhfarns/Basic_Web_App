f17fc16c6477bfd1e981db878d75bb5c3a729300
James Farnsworth <jfarnsworth@securitymetrics.com>
2017-09-11 20:09:47 -0600

    lots of stuff

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 from os import urandom
+import random
+import sys
+from binascii import hexlify
 
 def random_string(length = 16):
-    return urandom(length / 2).encode('hex')
+    return str(hexlify(urandom(length // 2)), 'ascii')
+
+def random_number():
+    key_num = random.SystemRandom()
+    return key_num.randint(0, 999999999999999999)
--- webapp.py
+++ webapp.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-from bottle import get, post, route, run, request, response, template
-from utils import random_string
+from bottle import get, post, route, run, request, response, template, redirect
+from utils import random_string, random_number
+
+users = {92:'values', 1: 'this is the voice'}
+
+users1 = {}
 
 def generate_cookie():
     return random_string(16)
 
+def generate_number():
+    return random_number()
 
 @route('/hello')
 @route('/')
@@ -24,6 +30,9 @@ def create_user_page():
 
 @get('/web/user/<id:int>')
 def get_user(id):
+
+    return users1[id]
+
     # TODO(james):
     # 1) look up the user in the in-memory dict using the id you got from
     #    bottle.
@@ -41,6 +50,17 @@ def create_user():
     email = request.forms.get('email')
     username = request.forms.get('username')
     password = request.forms.get('password')
+    user_id = generate_number()
+
+    global users1 
+
+    users1.setdefault(user_id, [])
+    
+    for addData in (firstname, lastname, email, username, password):
+        users1[user_id].append(addData)
+
+
+    return redirect('/web/user/' + str(user_id))
 
     # TODO(james):
     # 1) generate a random id for the new user
@@ -50,4 +70,4 @@ def create_user():
     #    page for that user.
 
 
-run(host='localhost', port=8080, debug=True)
+run(host='0.0.0.0', port=8080, debug=True)
